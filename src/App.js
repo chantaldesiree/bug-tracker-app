64b53fa004458583749f5f7665565d5b3a51c246
chantaldesiree <chantaldesiree@outlook.com>
2021-07-17 00:42:05 -0500

    Created Sign In and Sign Up forms.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
+import { Container } from "react-bootstrap";
+
+import SignUp from "./containers/SignUp";
+import SignIn from "./containers/SignIn";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <SignUp />
+      </div>
+    </Container>
   );
 }
-
-export default App;
